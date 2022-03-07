94ca23372b0f98ce78fb0a879ba531ce0f0f2241
Kaylan Prophet <kaylan.prophet@gmail.com>
2022-03-06 23:01:14 -0500

    added comments

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function getRandomItem(list) {
 // this function is going to take our users input
 function generatePassword() {
 
-//loop 
+//collect user input till it is validated or exit the prompts
 while (true) {
 
  var userInput = window.prompt('What do you want the length for your password to be?')
@@ -56,17 +56,20 @@ while (true) {
 }
 
 
-
+//prompts for the user to select characters for password
  var userWantsNumbers = window.confirm('Would you like numbers in your password?')
  var userWantsSymbols = window.confirm('Would you like symbols in your password?')
  var userWantsLowercase = window.confirm('Would you like lowerase letters in your password?')
  var userWantsUppercase = window.confirm('Would you like uppercase in your password?')
 
+ //list of available character types
  var numberList = ["0","1","2","3","4","5","6","7","8","9"]
  var symbolList = ["!"."@","#","$","%","&","?"]
  var lowercaseList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
  var uppercaseList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
+
+ //options cart to compile the selected list from the user
  var optionsCart = []
 
  if (userWantsNumbers === true) {
@@ -87,6 +90,8 @@ while (true) {
 
  if (optionsCart.length === 0)
   optionsCart.push(lowercaseList)
+
+
 
  var generatePassword = ""
 
